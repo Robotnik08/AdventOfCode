8e67e0d5662bb07df6e8e325aae271e57d0efc8d
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2022-12-02 23:16:57 +0100

    ewrrew

--- public/Day1/main.js
+++ public/Day1/main.js
@@ -4,18 +4,27 @@ fetch("input.txt")
 .then(r => {
     const result = r.split('\r\n');
     let highest = {c: 0, i: 0};
+    let second = {c: 0, i:0};
+    let third = {c: 0, i:0};
     let count = 0;
     for (let i in result) {
         if (result[i] > 0) {
             count += parseInt(result[i]);
         } else {
             if (count > highest.c) {
+                third = second;
+                second = highest;
                 highest = {c: count, i: i};
+            } else if (count > second.c) {
+                third = second;
+                second = {c: count, i: i};
+            } else if (count > third.c) {
+                third = {c: count, i: i};
             }
             count = 0;
         }
     }
-    console.log(highest);
+    console.log(highest.c + second.c + third.c);
 }
 );
 console.timeEnd('time');
