0bb0aadce9571c2b61b4989a49adcd44fe1f4f3d
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2022-12-05 22:40:17 +0100

    Day5 cleanup

--- public/Day5/day5.py
+++ public/Day5/day5.py
@@ -16,22 +16,11 @@ def parseInstructions(s):
     del(x[:10])
     return x
 
-def getResults1(boxes, instructions):
+def getResults(boxes, instructions,flip):
     for i in instructions:
         parsedInstruct = ''.join(filter(str.isdigit, i))
         appendBox = boxes[int(parsedInstruct[len(parsedInstruct) - 2]) - 1][len(boxes[int(parsedInstruct[len(parsedInstruct) - 2]) - 1]) - int(parsedInstruct[:len(parsedInstruct) - 2]):]
-        appendBox.reverse()
-        del(boxes[int(parsedInstruct[len(parsedInstruct) - 2]) - 1][len(boxes[int(parsedInstruct[len(parsedInstruct) - 2]) - 1]) - int(parsedInstruct[:len(parsedInstruct) - 2]):])
-        boxes[int(parsedInstruct[len(parsedInstruct) - 1]) -1].extend(appendBox)
-    res = ''
-    for i in boxes:
-        res += i[len(i) - 1]
-    return res
-
-def getResults2(boxes, instructions):
-    for i in instructions:
-        parsedInstruct = ''.join(filter(str.isdigit, i))
-        appendBox = boxes[int(parsedInstruct[len(parsedInstruct) - 2]) - 1][len(boxes[int(parsedInstruct[len(parsedInstruct) - 2]) - 1]) - int(parsedInstruct[:len(parsedInstruct) - 2]):]
+        if flip: appendBox.reverse()
         del(boxes[int(parsedInstruct[len(parsedInstruct) - 2]) - 1][len(boxes[int(parsedInstruct[len(parsedInstruct) - 2]) - 1]) - int(parsedInstruct[:len(parsedInstruct) - 2]):])
         boxes[int(parsedInstruct[len(parsedInstruct) - 1]) -1].extend(appendBox)
     res = ''
@@ -46,6 +35,6 @@ content = f.read()
 f.close()
 boxeslist = parseBox(content)
 instructionslist = parseInstructions(content)
-print('Result 1: ' + getResults1(boxeslist, instructionslist) + '\n')
+print('Result 1: ' + getResults(boxeslist, instructionslist, True) + '\n')
 boxeslist = parseBox(content)
-print('Result 2: ' + getResults2(boxeslist, instructionslist) + '\n')
+print('Result 2: ' + getResults(boxeslist, instructionslist, False) + '\n')
